84b2108eab39bf9603d4f53af2a2fe3a8e87cc08
Ibrahim <ibrachar68@gmail.com>
2022-12-12 17:28:08 +0100

    fine secondo footer

--- src/components/SecondFooterApp.vue
+++ src/components/SecondFooterApp.vue
@@ -2,6 +2,28 @@
 export default {
     data() {
         return {
+            imgSocial: [
+                {
+                    name: "Facebook",
+                    img: "/images/footer-facebook.png"
+                },
+                {
+                    name: "Twitter",
+                    img: "/images/footer-twitter.png"
+                },
+                {
+                    name: "Youtube",
+                    img: "/images/footer-youtube.png"
+                },
+                {
+                    name: "Pinterest",
+                    img: "/images/footer-pinterest.png"
+                },
+                {
+                    name: "Periscope",
+                    img: "/images/footer-periscope.png"
+                },
+            ]
 
         }
     }
@@ -11,7 +33,12 @@ export default {
 <template>
     <main>
         <div class="container">
+            <a href="">SIGN-UP NOW!</a>
+            <div>
+                <strong class="text-follow">FOLLOW US</strong>
+                <img class="social-img" :src="item.img" alt="" v-for="(item, index) in imgSocial" :key="index">
 
+            </div>
         </div>
     </main>
 
@@ -35,10 +62,31 @@ main {
         width: 60%;
         height: 100%;
         display: flex;
-        justify-content: space-evenly;
+        justify-content: space-between;
         align-items: center;
         color: white;
 
+        a {
+            text-decoration: none;
+            color: white;
+            font-weight: bold;
+            font-size: 18px;
+            border: 2px solid $color-type;
+            padding: 18px;
+
+        }
+
+        .text-follow {
+            color: $color-type;
+            font-size: 20px;
+            margin-right: 20px;
+        }
+
+        .social-img {
+            margin-right: 20px;
+            vertical-align: middle;
+            margin-bottom: 10px;
+        }
     }
 
 }
